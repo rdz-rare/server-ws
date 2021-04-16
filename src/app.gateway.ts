7833de5a2f74908c80eb8ed1786a03aeb7a89a3a
Angel Rodriguez <angel.rodriguez@gamanza.com>
2021-04-16 10:10:12 -0600

    Add ws module

---
+++ src/app.gateway.ts
@@ -0,0 +1,28 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  MessageBody,
+  OnGatewayInit,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Server } from 'ws';
+
+@WebSocketGateway(9090)
+export class AppGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('sendOnSiteNotification')
+  async eventOnsiteNotification(@MessageBody() data: any): Promise<WsResponse> {
+    console.log(data);
+    const message: WsResponse = {
+      event: 'sendOnSiteNotification',
+      data: {
+        a: 1,
+      },
+    };
+
+    return message;
+  }
+}
